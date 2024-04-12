5af7622d5bb29ada4b8bbe24959e69cc51099bb6
Markus Nyman <markus@nyman.dev>
2024-04-12 16:23:59 +0300

    fix: command opts

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,15 +1,14 @@
 local typst_watch = require("typst-watch")
 
 vim.api.nvim_buf_create_user_command(0, "TypstPreview", function(opts) typst_watch.open_preview(nil, opts.fargs[1]) end,
-    { desc = "Open compiled typst document", })
+    { nargs = "?", complete = "file", desc = "Open compiled typst document", })
 
 vim.api.nvim_buf_create_user_command(0, "TypstWatch",
     function(opts) typst_watch.watch(opts.fargs[1] or vim.fn.expand("%:p")) end,
     { nargs = "?", complete = "file", desc = "Start typst watch process", })
 
-vim.api.nvim_buf_create_user_command(0, "TypstWatchStop",
-    function(opts) typst_watch.stop() end,
-    { nargs = "?", complete = "file", desc = "Start typst watch process", })
+vim.api.nvim_buf_create_user_command(0, "TypstWatchStop", function() typst_watch.stop() end,
+    { desc = "Stop typst watch process", })
 
 if not vim.g.did_typst_watch_initialize then
     local main = vim.fn.findfile("main.typ", ".;")
